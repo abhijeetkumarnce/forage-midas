efa0b324008d35c169030940ba3474fa67add181
abhijeetkumarnce <abhijeetkeshri18@gmail.com>
2025-11-05 00:54:17 +0530

    Added classes and interfaces

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -2,8 +2,10 @@ package com.jpmc.midascore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MidasCoreApplication {
 
     public static void main(String[] args) {
